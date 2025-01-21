2b3ae6fa1dee5ea8affe7e60763d845821217604
Gabriel Panza <gabrieljpanza@gmail.com>
2025-01-21 12:04:59 -0300

    Update HUD
    - Trabalho em progresso. Tentando alinhar a ~div~ do botao

--- globalGameJam-2025/hud.gd
+++ globalGameJam-2025/hud.gd
@@ -5,10 +5,16 @@ var player
 var camera_path: NodePath = "/root/GameScene/Player/Camera2D"
 var camera
 var hud_offset: Vector2 = global_position
+var level_up_popup
 
 func _ready() -> void:
 	player = get_node_or_null(player_path)
 	camera = get_node_or_null(camera_path)
+	
+	level_up_popup = preload("res://LevelUpPopup.tscn").instantiate()
+	add_child(level_up_popup)
+	level_up_popup.connect("option_selected", Callable(self, "_apply_effect"))
+	
 	if player and camera:
 		# Salvar a posição inicial do HUD
 		hud_offset = global_position - camera.global_position
@@ -16,6 +22,7 @@ func _ready() -> void:
 		# Conecte os sinais do jogador ao HUD
 		player.connect("xp_updated", Callable(self, "_on_xp_updated"))
 		player.connect("level_updated", Callable(self, "_on_level_updated"))
+		player.connect("level_updated", Callable(self, "_on_level_up"))
 	else:
 		print("Jogador ou câmera não encontrados!")
 
@@ -27,3 +34,15 @@ func _on_level_updated(level, current_xp, xp_to_next_level) -> void:
 	$Label.text = "Level: %d" % level
 	$ProgressBar.value = current_xp
 	$ProgressBar.max_value = xp_to_next_level
+
+func _on_level_up(level, current_xp, xp_to_next_level):
+	level_up_popup.show_popup()
+
+func _apply_effect(option):
+	match option:
+		"option_1":
+			player.speed += 10
+		"option_2":
+			player.hp += 10
+		"option_3":
+			player.damage += 10
