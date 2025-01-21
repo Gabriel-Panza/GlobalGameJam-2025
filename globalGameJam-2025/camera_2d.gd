2192d2b5832918f1e057a0187229adf61065f96b
Gabriel Panza <gabrieljpanza@gmail.com>
2025-01-20 22:40:00 -0300

    PlayerMovement working

--- globalGameJam-2025/camera_2d.gd
+++ globalGameJam-2025/camera_2d.gd
@@ -3,16 +3,12 @@ extends Camera2D
 # Define os limites da câmera
 @export var limitLeft: int = 0
 @export var limitTop: int = 0
-@export var limitRight: int = $"../../Sprite2D".texture.get_width()
-@export var limitBottom: int = $"../../Sprite2D".texture.get_height()
+@export var limitRight: int = 1200
+@export var limitBottom: int = 0
 
 func _ready() -> void:
 	limit_left = limitLeft
 	limit_top = limitTop
 	limit_right = limitRight
 	limit_bottom = limitBottom
-	
 	enabled = true
-
-func _process(delta: float) -> void:
-	pass
--- globalGameJam-2025/player.gd
+++ globalGameJam-2025/player.gd
@@ -3,18 +3,26 @@ extends CharacterBody2D
 var speed := 200
 var velocity_vector := Vector2.ZERO
 
-var fundo_width: int = $"../../Sprite2D".texture.get_width()
-var fundo_height: int = $"../../Sprite2D".texture.get_height()
+var fundo_width: int = 0
+var fundo_height: int = 0
+
+func _ready() -> void:
+	var sprite_node = $"../Sprite2D"
+	if sprite_node and sprite_node.texture:
+		fundo_width = sprite_node.texture.get_width()*9.35
+		fundo_height = sprite_node.texture.get_height()*5
 
 func _process(delta: float) -> void:
+	velocity_vector.x = 0
+	velocity_vector.y = 0
 	if Input.is_action_pressed("ui_right"):
-		velocity_vector.x += 1
+		velocity_vector.x += 300
 	if Input.is_action_pressed("ui_left"):
-		velocity_vector.x -= 1
+		velocity_vector.x -= 300
 	if Input.is_action_pressed("ui_down"):
-		velocity_vector.y += 1
+		velocity_vector.y += 300
 	if Input.is_action_pressed("ui_up"):
-		velocity_vector.y -= 1
+		velocity_vector.y -= 300
 
 	velocity_vector = velocity_vector.normalized() * speed
 	var new_position = position + velocity_vector * delta
