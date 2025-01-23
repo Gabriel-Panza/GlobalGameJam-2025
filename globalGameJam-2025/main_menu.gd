8f5f763c5703245217c2f9c45505e97b569612c1
YuriSschinelli <yuri.schinelli@gmail.com>
2025-01-23 00:51:04 -0300

    menu finalizado
    opção "options" criada, com slider de volume completamente não-funcional e um placeholder de fundo bem cafona pra dar vibe de menu

--- globalGameJam-2025/main_menu.gd
+++ globalGameJam-2025/main_menu.gd
@@ -5,3 +5,7 @@ func _on_start_game_pressed() -> void:
 
 func _on_button_2_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_options_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://options_menu.tscn")
