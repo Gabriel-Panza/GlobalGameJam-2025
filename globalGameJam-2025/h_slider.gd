f30a8a87d7afcfe2d9a8c4bc244c2c8c8cdf5875
Gabriel Panza <gabrieljpanza@gmail.com>
2025-01-23 10:08:31 -0300

    Adjusted Menu Placeholder

--- globalGameJam-2025/h_slider.gd
+++ globalGameJam-2025/h_slider.gd
@@ -1,10 +1,4 @@
 extends HSlider
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	var texto = get_child(0)
-	texto.text = var_to_str(value)
-
-
 func _on_return_pressed() -> void:
 	get_tree().change_scene_to_file("res://main_menu.tscn")
