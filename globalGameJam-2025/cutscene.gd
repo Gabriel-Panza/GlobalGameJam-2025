37b1183476eb1bd3e0ed433b5f3e1b00a7304bac
YuriSschinelli <yuri.schinelli@gmail.com>
2025-01-26 16:02:28 -0300

    cutscene skip adicionado

--- globalGameJam-2025/cutscene.gd
+++ globalGameJam-2025/cutscene.gd
@@ -3,5 +3,9 @@ extends VideoStreamPlayer
 func _ready():
 	connect("finished", Callable(self, "_on_video_finished"))
 
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		_on_video_finished()
+
 func _on_video_finished():
 	get_tree().change_scene_to_file("res://tela_inicial.tscn")
