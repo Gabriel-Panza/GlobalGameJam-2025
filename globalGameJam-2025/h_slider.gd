76592161263a48db2b7e8d9251ac738739b2c809
YuriSschinelli <yuri.schinelli@gmail.com>
2025-01-25 21:35:31 -0300

    Criado o slider de volume

--- globalGameJam-2025/h_slider.gd
+++ globalGameJam-2025/h_slider.gd
@@ -8,3 +8,9 @@ func _process(delta: float) -> void:
 	texto1.text = var_to_str(value)
 	var texto2 = $"../Volume2"
 	texto2.text = var_to_str($"../HSlider2".value)
+
+
+func _on_value_changed(value: float) -> void:
+	var musica = get_parent().get_parent().get_node_or_null("AudioStreamPlayer2D")
+	musica.set_volume_db(value)
+	
